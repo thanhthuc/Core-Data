efdb767a5627ee0d38ca9eff56629defd90a839a
Thanh Thuc <thanhthucbkinfo@yahoo.com.vn>
2017-10-28 12:59:01 +0700

    Create project

---
+++ Core-Data/Core-Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core-Data
+//
+//  Created by Thuc on 10/28/17.
+//  Copyright © 2017 Thuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
