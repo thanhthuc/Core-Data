5e67bb95b2f16336698cd2d7085fc930bc60a280
Thanh Thuc <thanhthucbkinfo@yahoo.com.vn>
2017-11-12 22:57:37 +0700

    Completed create model with relationship

---
+++ Core-Data/Core-Data/Model/Employee.swift
@@ -0,0 +1,15 @@
+//
+//  Employee.swift
+//  Core-Data
+//
+//  Created by Thuc on 11/12/17.
+//  Copyright © 2017 Thuc. All rights reserved.
+//
+import Foundation
+import CoreData
+
+class Employee: NSManagedObject {
+    @NSManaged var firstName: String?
+    @NSManaged var lastName: String?
+    @NSManaged var shoutOuts: NSSet?
+}
---
+++ Core-Data/Core-Data/Model/Shoutout.swift
@@ -0,0 +1,18 @@
+//
+//  Shoutout.swift
+//  Core-Data
+//
+//  Created by Thuc on 11/12/17.
+//  Copyright © 2017 Thuc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ShoutOut: NSManagedObject {
+    @NSManaged var from: String?
+    @NSManaged var message: String?
+    @NSManaged var shoutCategory: String
+    @NSManaged var sentDate: Date?
+    @NSManaged var toEmployee: Employee
+}
